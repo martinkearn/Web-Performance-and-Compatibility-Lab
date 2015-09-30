18fc9e865ffc088536fb058c79620c27bdc47676
Martin Kearn <martin.kearn@microsoft.com>
2015-09-30 10:31:36 +0100

    Planted incompatible code

---
+++ design/js/site.js
@@ -0,0 +1,8 @@
+if (!Modernizr.svg) {
+	document.getElementById('logo').src = 'images/Microsoft_logo.png';
+}
+
+/*if (navigator.userAgent.indexOf("MSIE") > 0)
+{
+	document.getElementById('logo').src = 'images/Microsoft_logo.png';
+}*/
