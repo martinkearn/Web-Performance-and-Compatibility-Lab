f716384414e2e7630bf9fafe269ab8bdd47c05de
Martin Kearn <martin.kearn@microsoft.com>
2015-09-30 11:00:13 +0100

    Added css transition for just webkit

--- design/js/site.js
+++ design/js/site.js
@@ -2,3 +2,7 @@ if (navigator.userAgent.indexOf("MSIE") > 0)
 {
 	document.getElementById('logo').src = 'images/Microsoft_logo.png';
 }
+
+        if (!Modernizr.svg) {
+            document.getElementById('logo').src = '/Content/Microsoft_logo.png';
+        }
