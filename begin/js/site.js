245533e577e771f9b0f77e8918ba81ac0fef148a
Martin Kearn <martin.kearn@microsoft.com>
2015-09-30 10:48:03 +0100

    Initial 'begin' code
    With all the bad stuff in

--- begin/js/site.js
+++ begin/js/site.js
@@ -1,3 +1,7 @@
+if (!Modernizr.svg) {
+	document.getElementById('logo').src = 'images/Microsoft_logo.png';
+}
+
 if (navigator.userAgent.indexOf("MSIE") > 0)
 {
 	document.getElementById('logo').src = 'images/Microsoft_logo.png';
