9c23cb8f746d2171a27d5893cc6bb0619ed5eced
Martin Kearn <martin.kearn@microsoft.com>
2015-09-30 10:44:26 +0100

    Edge doc mode issue

--- design/js/site.js
+++ design/js/site.js
@@ -2,7 +2,7 @@ if (!Modernizr.svg) {
 	document.getElementById('logo').src = 'images/Microsoft_logo.png';
 }
 
-/*if (navigator.userAgent.indexOf("MSIE") > 0)
+if (navigator.userAgent.indexOf("MSIE") > 0)
 {
 	document.getElementById('logo').src = 'images/Microsoft_logo.png';
-}*/
+}
